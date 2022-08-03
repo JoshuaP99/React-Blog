3adf44b878739a33e5736c6c1b87b15c8c1c13b4
Joshua Pacheco <pikminguy56@gmail.com>
2022-08-03 14:21:59 -0600

    Final Upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import NewPost from './components/NewPost';
+import PostPage from './components/PostPage';
+import About from './components/About';
+import Missing from './components/Missing';
+import { Route, Switch, useHistory } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { format } from 'date-fns';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "My First Post",
+      dateTime: "July 28, 2022 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 2,
+      title: "My Second Post",
+      dateTime: "July 28, 2022 12:32:22 PM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+  ])
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [postTitle, setPostTitle] = useState('');
+  const [postBody, setPostBody] = useState('');
+  const history = useHistory();
+
+  useEffect(() => {
+    const filteredResults = posts.filter(post =>
+      ((post.body).toLowerCase()).includes(search.toLowerCase())
+      || ((post.title).toLowerCase()).includes(search.toLowerCase()));
+
+      setSearchResults(filteredResults.reverse())
+  },[posts, search])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const dateTime = format(new Date(), "MMMM dd, yyyy pp")
+    const newPost = { id, title: postTitle, dateTime, body: postBody };
+    const allPosts = [ ...posts, newPost ]
+    setPosts(allPosts);
+    setPostTitle('');
+    setPostBody('');
+    history.push('/');
+  }
+
+  const handleDelete = (id) => {
+    const postsList = posts.filter(post => post.id !== id);
+    setPosts(postsList);
+    history.push('/')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header title={"Josh's Blog"} />
+      <Nav search={search} setSearch={setSearch}/>
+      <Switch>
+        <Route exact path='/'>
+          <Home posts={searchResults}/>
+        </Route>
+        <Route exact path='/post'>
+          <NewPost
+            handleSubmit={handleSubmit}
+            postTitle={postTitle}
+            setPostTitle={setPostTitle}
+            postBody={postBody}
+            setPostBody={setPostBody}
+           />
+        </Route>
+        <Route path='/post/:id'>
+          <PostPage posts={posts} handleDelete={handleDelete}/>
+        </Route>
+        <Route path='/about' component={About} />
+        <Route path='*' component={Missing} />
+      </Switch>
+      <Footer />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+const About = () => {
+    return (
+        <main className="About">
+            <h2>About</h2>
+            <p style={{ marginTop: '1rem' }}>
+                Hello! Welcome to my final project page! 
+                This project was created as a final for my front end 
+                development course that uses all the skills we had 
+                learned throughout our bootcamp. This project is a 
+                basic social media platform where you can make posts 
+                and delete them from the site. The site also features a 
+                fully functional navigation bar with routing to different 
+                pages on the site. I hope you enjoy looking at the site and 
+                feel free to send me any questions on GitHub or LinkedIn!
+            </p>
+        </main>
+    )
+}
+
+export default About
